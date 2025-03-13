ed8f9b2d0b2dd880d464e9592afeb8042ebf9cd2
tejas git <d9489079@gmail.com>
2025-03-13 15:39:53 +0530

    second commit

---
+++ Language_Fundamentals/src/tutorial/funfamental/language/identifiers/IdentifierClassName.java
@@ -0,0 +1,10 @@
+package tutorial.funfamental.language.identifiers;
+
+public class IdentifierClassName {
+	public static void main(String[] args) {
+		int String = 888;
+		int Runnable = 999;
+		System.out.println("String " + String);
+		System.out.println("Runnable " + Runnable);
+	}
+}
